7b3ee95093f6160df41cd140a5c1395408c8b61e
sandarva3 <sandarvapoudel182@gmail.com>
2025-01-07 18:38:50 +0545

    login, logout, register

--- callapp/urls.py
+++ callapp/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .views import(
     home_view,
     register_view,
+    logout_view,
 )
 urlpatterns = [
     path('', home_view, name="home"),
-    path('register/', register_view, name='register')
+    path('register/', register_view, name='register'),
+    path('logout/', logout_view, name="logout"),
 ]
--- callapp/views.py
+++ callapp/views.py
@@ -1,19 +1,53 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth import authenticate, login, logout
 from .models import User
 
 def home_view(request):
     if request.method == "POST":
-        print("FORM is Submitted.")
+
+        print("Login FORM is Submitted.")
         username = request.POST.get('username')
         password = request.POST.get('password')
-        print(f"The username is: {username}")
-        print(f"The password is: {password}")
-        
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            print("LOGGED In")
+            msg = "You're currently Logged in."
+            return render(request, 'login.html', {'loginMsg': msg})
+        else:
+            msg = "Invalid username and password."
+            return render(request, 'login.html', {'msg': msg})
     else:
         print("FORM IS NOT SUBMITTED")
     return render(request, 'login.html')
 
 
 def register_view(request):
+    if request.method == "POST":
+        print("Register FORM is submitted.")
+
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm-password')
+
+        if password != confirm_password:
+            msg = "Two password fields didn't matched."
+            return render(request, 'register.html', {'msg': msg})
+        
+        if User.objects.filter(username=username).exists():
+            msg = "User with that username already exists."
+            return render(request, 'register.html', {'msg': msg})
+        
+        User.objects.create_user(username=username, email=email, password=password)
+        print("New user created.")
+        return redirect('home')
+    
     return render(request, 'register.html')
+
+
+def logout_view(request):
+    logout(request)
+    print("Logged out.")
+    return redirect('home')
