9ce4535b9ccbd5c4dd2a61912f66a6f4e864d5fb
sandarva3 <sandarvapoudel182@gmail.com>
2025-01-09 15:27:35 +0545

    call- partial

--- callapp/urls.py
+++ callapp/urls.py
@@ -3,9 +3,13 @@ from .views import(
     home_view,
     register_view,
     logout_view,
+    start_call_view,
+    signaling_server_view
 )
 urlpatterns = [
     path('', home_view, name="home"),
-    path('register/', register_view, name='register'),
+    path('register/', register_view, name="register"),
     path('logout/', logout_view, name="logout"),
+    path('startCall/<int:calleID>/', start_call_view, name="startCall"),
+    path('signaling/', signaling_server_view, name="signalingServer")
 ]
--- callapp/views.py
+++ callapp/views.py
@@ -1,8 +1,56 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth import authenticate, login, logout
 from .models import User
+from django.http import JsonResponse
+import json
+
 
 #online_users = []
+
+'''
+startcall function
+Takes calleID as parameter. Check If the request method is post and if the user is authenticated. get the Callee form passed id. If callee is found then 
+Return some json response(not needed compulsorily though.), else return message with calle not found.
+'''
+def start_call_view(request, calle_id):
+    if request.method == "POST" and request.user.is_authenticated:
+        calle = User.objects.get(id=calle_id)
+        if calle:
+            return JsonResponse({
+                'msg': "Call initiated",
+                'caller': request.user.username
+            })
+        else:
+            return JsonResponse({
+                'msg': "Callee not found."
+            },status=400)
+    else:
+        #return HttpResponse("BAD Gateway. Try again.", status=400)
+        return JsonResponse({
+            'msg': "BAD Gateway."
+        }, status=400)
+
+'''
+signaling server. We get json request here.
+chec if the request is post and user is authenticated. get the data by loading it from json data from request.body. the data will contain the calleID, get the 
+callee through that id. Get sdp and iceCandidate as well. Just sent the jsonResponse saying anything.
+'''
+def signaling_server_view(request):
+    if request.method == "POST" and request.user.is_authenticated:
+        data = json.loads(request.body)
+        calle_id = data.get('calleID')
+        sdp = data.get('sdp')
+        ice_candidate = data.get('iceCandidate')
+        calle = User.objects.get(id=calle_id)
+        
+        return JsonResponse({
+            'msg': 'Signaling server received the data.'
+        })
+    else:
+        return JsonResponse({
+            'msg': 'BAD request.'
+        }, status=400)
+
 
 def home_view(request):
     if request.method == "POST":
